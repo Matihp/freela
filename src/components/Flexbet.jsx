fa6607775a6ca590f832c44f0df4290bc848d2e4
matihp <matiasleoncontreras@gmail.com>
2024-01-21 20:40:22 -0300

    flexbox

--- src/components/Flexbet.jsx
+++ src/components/Flexbet.jsx
@@ -3,11 +3,11 @@ import logo from './../assets/logo.png'
 
 const Flexbet = () => {
   return (
-    <div className='flex'>
-        <div className='h-40'>
+    <div className='flex '>
+        <div className='h-40 w-40'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque pariatur voluptatum reiciendis delectus quisquam. Sapiente consequuntur tempore sed odit asperiores suscipit blanditiis? Dolores, voluptatem facilis fugiat rerum cumque ad possimus?
         </div>
-        <img style={{height:"10vh",width:"10vw"}} src={logo} alt="" />
+        <img className=' h-20' src={logo} alt="" />
     </div>
     
   )
