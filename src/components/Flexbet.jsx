6c2276244ae56f4e02b8fc0d079cd58f2d793d06
matihp <matiasleoncontreras@gmail.com>
2024-02-17 21:49:51 -0300

    slider slick

--- src/components/Flexbet.jsx
+++ src/components/Flexbet.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import logo from './../assets/logo.png'
+import fondo from '../assets/fondo.png'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 
 const Flexbet = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2
+  };
+
   return (
-    <div className='flex '>
-        <div className='h-40 w-40'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque pariatur voluptatum reiciendis delectus quisquam. Sapiente consequuntur tempore sed odit asperiores suscipit blanditiis? Dolores, voluptatem facilis fugiat rerum cumque ad possimus?
-        </div>
-        <img className=' h-20' src={logo} alt="" />
+    <div className=" w-150 text-white">
+      <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+      <div>
+        <h3>4</h3>
+      </div>
+      <div>
+        <h3>5</h3>
+      </div>
+      <div>
+        <h3>6</h3>
+      </div>
+      </Slider>
     </div>
     
   )
